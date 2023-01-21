ee80d383f76edb0cd59ced2ae864fef2f846306a
kickASS28 <ashwamedhpb@gmail.com>
2023-01-21 19:57:07 +0530

    comentted jade

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
